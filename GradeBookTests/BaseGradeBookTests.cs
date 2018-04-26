b57716233b5c85559c49b2a632d823ba9783b8ce
Joe Lachovsky <jlachovsky@gmail.com>
2018-04-26 15:03:52 -0400

    Update StartingUserInterface.HelpCommand Method

---
+++ GradeBookTests/BaseGradeBookTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using GradeBook.GradeBooks;
+using Xunit;
+
+namespace GradeBookTests
+{
+    public class BaseGradeBookTests
+    {
+        [Fact]
+        public void GivenNoGradesInGradeList_ThenDisplayNoGradesAvailable()
+        {
+
+        }
+        //Arrange
+        //Act
+        //Assert
+    }
+}
